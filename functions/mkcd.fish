890f2d39ecbd189545599bda55e5be68a09345aa
Drake <mdrakea3@tutanota.com>
2021-01-23 19:59:14 -0500

    fix /es and spaces in paths

--- functions/mkcd.fish
+++ functions/mkcd.fish
@@ -1,9 +1,13 @@
 function mkcd --description "Make a directory, and automatically cd into it."
-    mkdir $argv
-    cd $argv > ~/.tmp/mkcd.txt 2> ~/.tmp/mkcd.txt
-    if grep -i "Too many args for cd command" ~/.tmp/mkcd.txt
-        echo mkcd Error: Too many directories specified
-        rm -r $argv
+    set alldirs (string split ' ' (string split '/' $argv))
+    echo $alldirs
+    for path in $alldirs
+        mkdir $path
+        cd $path > ~/.tmp/mkcd.txt 2> ~/.tmp/mkcd.txt
+        if grep -i "Too many args for cd command" ~/.tmp/mkcd.txt
+            echo mkcd Error: Too many directories specified
+            rm -r $path
+        end
+        rm ~/.tmp/mkcd.txt
     end
-    rm ~/.tmp/mkcd.txt
 end
